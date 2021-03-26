1450c04ebb0e76ea4cdf322ca982f5af1b03dd29
lamine messaci <lamine.messaci18@gmail.com>
2021-03-26 15:22:19 +0100

    installing Font expo

--- App.js
+++ App.js
@@ -1,7 +1,27 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Text, View } from "react-native";
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={console.log("impossile de telecharger les fonts !")}
+      />
+    );
+  }
   return (
     <View>
       <Text>Open up App.js to start working on your app!</Text>
