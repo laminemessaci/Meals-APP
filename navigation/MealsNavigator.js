f23a251db19e24c5b54fde13952ac57c7d1fbfb9
lamine messaci <lamine.messaci18@gmail.com>
2021-03-27 16:44:14 +0100

    Add tabNav an DrawerNAv import

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,6 +1,8 @@
 import { Platform } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import { createDrawerNavigator } from "react-navigation-drawer";
 
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
