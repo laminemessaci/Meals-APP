0bc32ca5b6c433161681bc0aac09d23fbc23d71f
lamine messaci <lamine.messaci18@gmail.com>
2021-03-27 20:44:14 +0100

    Create bottomTabNAvigation

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,13 +1,17 @@
+import React from "react";
 import { Platform } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import { createDrawerNavigator } from "react-navigation-drawer";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
 import MealDetailScreen from "../screens/MealDetailScreen";
+import FavoritesScreen from "../screens/FavoritesScreen";
 import Colors from "../constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
 
 const MealsNavigator = createStackNavigator(
   {
@@ -32,4 +36,41 @@ const MealsNavigator = createStackNavigator(
   }
 );
 
-export default createAppContainer(MealsNavigator);
+const MealsFavNavigator = createBottomTabNavigator(
+  {
+    Meals: {
+      screen: MealsNavigator,
+      navigationOptions: {
+        tabBarIcon: (tabInfo) => {
+          return (
+            <Ionicons
+              name="ios-restaurant"
+              size={24}
+              color={tabInfo.tintColor}
+            />
+          );
+        },
+        tabBarColor: Colors.primaryColor,
+      },
+    },
+    Favorites: {
+      screen: FavoritesScreen,
+      navigationOptions: {
+        tabBarLabel: "Favorites!",
+        tabBarIcon: (tabInfo) => {
+          return (
+            <Ionicons name="ios-star" size={24} color={tabInfo.tintColor} />
+          );
+        },
+      },
+      tabBarColor: Colors.accentColor,
+    },
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: Colors.accentColor,
+    },
+  }
+);
+
+export default createAppContainer(MealsFavNavigator);
