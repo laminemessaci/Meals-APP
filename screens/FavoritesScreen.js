531f2df5d2cf3addc8f7f00e45c8a6b422a8eaa0
lamine messaci <lamine.messaci18@gmail.com>
2021-03-28 17:21:45 +0200

    Configure Filter Meals

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,22 +1,49 @@
-import React from "react";
-import { StyleSheet } from "react-native";
-import MealList from "../components/MealList";
-import { useSelector } from "react-redux";
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import { useSelector } from 'react-redux';
 
-const FavoritesScreen = (props) => {
-  const favMeal = useSelector((state) => state.meals.favoriteMeals);
-  /*  const favMeal = availableMeals.filter(
-    (meal) => meal.id === "m1" || meal.id === "m2"
-  ); */
-  return <MealList listData={favMeal} navigation={props.navigation} />;
+import HeaderButton from '../components/HeaderButton';
+import MealList from '../components/MealList';
+import DefaultText from '../components/DefaultText';
+
+const FavoritesScreen = props => {
+  const favMeals = useSelector(state => state.meals.favoriteMeals);
+
+  if (favMeals.length === 0 || !favMeals) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No favorite meals found. Start adding some!</DefaultText>
+      </View>
+    );
+  }
+
+  return <MealList listData={favMeals} navigation={props.navigation} />;
+};
+
+FavoritesScreen.navigationOptions = navData => {
+  return {
+    headerTitle: 'Your Favorites',
+    headerLeft: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Menu"
+          iconName="ios-menu"
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    )
+  };
 };
 
 const styles = StyleSheet.create({
-  screen: {
+  content: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 });
 
 export default FavoritesScreen;
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,13 +1,13 @@
-import React, { useEffect, useCallback } from "react";
-import { ScrollView, View, Image, Text, StyleSheet } from "react-native";
-import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import React, { useEffect, useCallback } from 'react';
+import { ScrollView, View, Image, Text, StyleSheet } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import { useSelector, useDispatch } from 'react-redux';
 
-import { useSelector, useDispatch } from "react-redux";
-import HeaderButton from "../components/HeaderButton";
-import DefaultText from "../components/DefaultText";
-import { toggleFavorite } from "../store/action/meals";
+import HeaderButton from '../components/HeaderButton';
+import DefaultText from '../components/DefaultText';
+import { toggleFavorite } from '../store/actions/meals';
 
-const ListItem = (props) => {
+const ListItem = props => {
   return (
     <View style={styles.listItem}>
       <DefaultText>{props.children}</DefaultText>
@@ -15,11 +15,14 @@ const ListItem = (props) => {
   );
 };
 
-const MealDetailScreen = (props) => {
-  const availableMeals = useSelector((state) => state.meals.meals);
-  const mealId = props.navigation.getParam("mealId");
+const MealDetailScreen = props => {
+  const availableMeals = useSelector(state => state.meals.meals);
+  const mealId = props.navigation.getParam('mealId');
+  const currentMealIsFavorite = useSelector(state =>
+    state.meals.favoriteMeals.some(meal => meal.id === mealId)
+  );
 
-  const selectedMeal = availableMeals.find((meal) => meal.id === mealId);
+  const selectedMeal = availableMeals.find(meal => meal.id === mealId);
 
   const dispatch = useDispatch();
 
@@ -32,6 +35,10 @@ const MealDetailScreen = (props) => {
     props.navigation.setParams({ toggleFav: toggleFavoriteHandler });
   }, [toggleFavoriteHandler]);
 
+  useEffect(() => {
+    props.navigation.setParams({ isFav: currentMealIsFavorite });
+  }, [currentMealIsFavorite]);
+
   return (
     <ScrollView>
       <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
@@ -41,54 +48,59 @@ const MealDetailScreen = (props) => {
         <DefaultText>{selectedMeal.affordability.toUpperCase()}</DefaultText>
       </View>
       <Text style={styles.title}>Ingredients</Text>
-      {selectedMeal.ingredients.map((ingredient) => (
+      {selectedMeal.ingredients.map(ingredient => (
         <ListItem key={ingredient}>{ingredient}</ListItem>
       ))}
       <Text style={styles.title}>Steps</Text>
-      {selectedMeal.steps.map((step) => (
+      {selectedMeal.steps.map(step => (
         <ListItem key={step}>{step}</ListItem>
       ))}
     </ScrollView>
   );
 };
 
-MealDetailScreen.navigationOptions = (navigationData) => {
+MealDetailScreen.navigationOptions = navigationData => {
   // const mealId = navigationData.navigation.getParam('mealId');
-  const mealTitle = navigationData.navigation.getParam("mealTitle");
-  const toggleFavorite = navigationData.navigation.getParam("toggleFav");
+  const mealTitle = navigationData.navigation.getParam('mealTitle');
+  const toggleFavorite = navigationData.navigation.getParam('toggleFav');
+  const isFavorite = navigationData.navigation.getParam('isFav');
   // const selectedMeal = MEALS.find(meal => meal.id === mealId);
   return {
     headerTitle: mealTitle,
     headerRight: (
       <HeaderButtons HeaderButtonComponent={HeaderButton}>
-        <Item title="Favorite" iconName="ios-star" onPress={toggleFavorite} />
+        <Item
+          title="Favorite"
+          iconName={isFavorite ? 'ios-star' : 'ios-star-outline'}
+          onPress={toggleFavorite}
+        />
       </HeaderButtons>
-    ),
+    )
   };
 };
 
 const styles = StyleSheet.create({
   image: {
-    width: "100%",
-    height: 200,
+    width: '100%',
+    height: 200
   },
   details: {
-    flexDirection: "row",
+    flexDirection: 'row',
     padding: 15,
-    justifyContent: "space-around",
+    justifyContent: 'space-around'
   },
   title: {
-    fontFamily: "open-sans-bold",
+    fontFamily: 'open-sans-bold',
     fontSize: 22,
-    textAlign: "center",
+    textAlign: 'center'
   },
   listItem: {
     marginVertical: 10,
     marginHorizontal: 20,
-    borderColor: "#ccc",
+    borderColor: '#ccc',
     borderWidth: 1,
-    padding: 10,
-  },
+    padding: 10
+  }
 });
 
 export default MealDetailScreen;
